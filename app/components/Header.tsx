18271789b469c0dbc50025130fa34a5d2d713439
Xiang Li <phnophen.li@gmail.com>
2025-03-09 23:00:16 +0800

    prototype

---
+++ app/components/Header.tsx
@@ -0,0 +1,22 @@
+'use client';
+
+import Link from "next/link";
+import Image from "next/image";
+import styles from './Header.module.css';
+
+const Header = () => {
+    return (
+        <nav className={styles.navbar}>                    
+            <div className={styles.logo}>Trust and Comfort</div>
+            <ul className={styles.navLinks}>
+                <li><Link href="/investment"><span>Investment</span></Link></li>
+                <li><Link href="/loan"><span>Loan</span></Link></li>
+                <li><Link href="/hedging"><span>Hedging</span></Link></li>
+                <li><Link href="/smart-money"><span>Smart Money</span></Link></li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Header;
+
