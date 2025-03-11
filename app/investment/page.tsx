cc759753ee80d6dddbd19fbcb97ceb7a30ed74ab
Xiang Li <phnophen.li@gmail.com>
2025-03-12 00:49:10 +0800

    add loading

--- app/investment/page.tsx
+++ app/investment/page.tsx
@@ -15,52 +15,60 @@ interface Product {
 
 const Investment: React.FC = () => {
     const [products, setProducts] = useState<Product[]>([]);
+    const [loading, setLoading] = useState<boolean>(true);
 
     useEffect(() => {
         fetch('/api/investment_data')
           .then((response) => response.json())
-          .then((data) => setProducts(data));
+          .then((data) => {
+              setProducts(data);
+              setLoading(false);
+          });
     }, []);
 
     return (
         <div className={styles.productList}>
-            {products.map((product, index) => (
-                <div key={index} className={styles.productCard}>
-                    <table className={styles.productTable}>
-                        <thead className={styles.tableHeader}>
-                            <tr>
-                                <td className={styles.title}>{product.name}</td>
-                                <td></td>
-                            </tr>
-                        </thead>
-                        <tbody className={styles.tableBody}>
-                            <tr>
-                                <td className={styles.subtitle} style={{ padding: '2px 8px'}}>{product.source}</td>
-                            </tr>
-                            <tr>
-                                <td colSpan={2} style={{ height: '10px', backgroundColor: 'transparent' }}></td>
-                            </tr>
-                            <tr>
-                                <td style={{ padding: '0px 8px'}}>
-                                    APR <span className={styles.value } style={{color: 'green'}}>{product.apr}%</span>
-                                </td>
-                                <td>
-                                    Max drawdown <span className={styles.value } style={{color: 'red'}}>{product.drawdown}%</span>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td style={{ padding: '0px 8px'}}>
-                                    Strategy <span className={styles.value }>{product.strategy}</span>
-                                </td>
-                                <td>
-                                    Sub status <span className={styles.value }>{product.sub_status}</span>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
-                    <div className={styles.productDescription } style={{ padding: '0px 8px'}}>{product.description}</div>
-                </div>
-            ))}
+            {loading ? (
+                <div className={styles.loading}>LOADING...</div>
+            ) : (
+                products.map((product, index) => (
+                    <div key={index} className={styles.productCard}>
+                        <table className={styles.productTable}>
+                            <thead className={styles.tableHeader}>
+                                <tr>
+                                    <td className={styles.title}>{product.name}</td>
+                                    <td></td>
+                                </tr>
+                            </thead>
+                            <tbody className={styles.tableBody}>
+                                <tr>
+                                    <td className={styles.subtitle} style={{ padding: '2px 8px'}}>{product.source}</td>
+                                </tr>
+                                <tr>
+                                    <td colSpan={2} style={{ height: '10px', backgroundColor: 'transparent' }}></td>
+                                </tr>
+                                <tr>
+                                    <td style={{ padding: '0px 8px'}}>
+                                        APR <span className={styles.value } style={{color: 'green'}}>{product.apr}%</span>
+                                    </td>
+                                    <td>
+                                        Max drawdown <span className={styles.value } style={{color: 'red'}}>{product.drawdown}%</span>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td style={{ padding: '0px 8px'}}>
+                                        Strategy <span className={styles.value }>{product.strategy}</span>
+                                    </td>
+                                    <td>
+                                        Sub status <span className={styles.value }>{product.sub_status}</span>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        <div className={styles.productDescription } style={{ padding: '0px 8px'}}>{product.description}</div>
+                    </div>
+                ))
+            )}
             <div className={styles.invisiable}></div>
             <div className={styles.invisiable}></div>
         </div>
--- app/loan/page.tsx
+++ app/loan/page.tsx
@@ -15,52 +15,60 @@ interface Product {
 
 const LoanPage: React.FC = () => {
     const [products, setProducts] = useState<Product[]>([]);
+    const [loading, setLoading] = useState<boolean>(true);
 
     useEffect(() => {
         fetch('/api/loan_data')
           .then((response) => response.json())
-          .then((data) => setProducts(data));
+          .then((data) => {
+              setProducts(data);
+              setLoading(false);
+          });
     }, []);
 
     return (
         <div className={styles.productList}>
-            {products.map((product, index) => (
-                <div key={index} className={styles.productCard}>
-                    <table className={styles.productTable}>
-                        <thead className={styles.tableHeader}>
-                            <tr>
-                                <td className={styles.title}>{product.asset}</td>
-                                <td></td>
-                            </tr>
-                        </thead>
-                        <tbody className={styles.tableBody}>
-                            <tr>
-                                <td className={styles.subtitle} style={{ padding: '2px 8px'}}>{product.source}</td>
-                            </tr>
-                            <tr>
-                                <td colSpan={2} style={{ height: '10px', backgroundColor: 'transparent' }}></td>
-                            </tr>
-                            <tr>
-                                <td style={{ padding: '0px 8px'}}>
-                                    Interest <span className={styles.value} style={{ color: 'green'}}>{product.interest_rate}%</span> 
-                                </td>
-                                <td>
-                                    Mortgage <span className={styles.value} style={{ color: 'red'}}>{product.mortgage_rate}%</span>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td style={{ padding: '0px 8px'}}>
-                                    Amount <span className={styles.value}>{product.supply_amount}</span> 
-                                </td>
-                                <td>
-                                    Status <span className={styles.value}>{product.status}</span>             
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
-                    <div className={styles.productDescription} style={{ padding: '0px 8px'}}>{product.description}</div>
-                </div>
-            ))}
+            {loading ? (
+                <div className={styles.loading}>LOADING...</div>
+            ) : (
+                products.map((product, index) => (
+                    <div key={index} className={styles.productCard}>
+                        <table className={styles.productTable}>
+                            <thead className={styles.tableHeader}>
+                                <tr>
+                                    <td className={styles.title}>{product.asset}</td>
+                                    <td></td>
+                                </tr>
+                            </thead>
+                            <tbody className={styles.tableBody}>
+                                <tr>
+                                    <td className={styles.subtitle} style={{ padding: '2px 8px'}}>{product.source}</td>
+                                </tr>
+                                <tr>
+                                    <td colSpan={2} style={{ height: '10px', backgroundColor: 'transparent' }}></td>
+                                </tr>
+                                <tr>
+                                    <td style={{ padding: '0px 8px'}}>
+                                        Interest <span className={styles.value} style={{ color: 'green'}}>{product.interest_rate}%</span> 
+                                    </td>
+                                    <td>
+                                        Mortgage <span className={styles.value} style={{ color: 'red'}}>{product.mortgage_rate}%</span>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td style={{ padding: '0px 8px'}}>
+                                        Amount <span className={styles.value}>{product.supply_amount}</span> 
+                                    </td>
+                                    <td>
+                                        Status <span className={styles.value}>{product.status}</span>             
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        <div className={styles.productDescription} style={{ padding: '0px 8px'}}>{product.description}</div>
+                    </div>
+                ))
+            )}
             <div className={styles.invisiable}></div>
             <div className={styles.invisiable}></div>
         </div>
